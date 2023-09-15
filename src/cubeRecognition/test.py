125f44587541d98777261c5aa2f8f8a4362cf5d8
quentinformatique <quentin161204@gmail.com>
2023-09-15 13:33:04 +0200

    Update test.py

--- src/cubeRecognition/test.py
+++ src/cubeRecognition/test.py
@@ -1,25 +1,91 @@
 import cv2
+import numpy as np
 
+# Initialize the video capture
 vid = cv2.VideoCapture(0)
 
-
 def getColor(hue_value):
-    if hue_value < 5:
-        return "RED"
-    elif hue_value < 22:
-        return "ORANGE"
-    elif hue_value < 33:
-        return "YELLOW"
-    elif hue_value < 78:
-        return "GREEN"
-    elif hue_value < 131:
-        return "BLUE"
-    elif hue_value < 180:
-        return "WHITE"
+    # Define hue value ranges for each color
+    color_ranges = {
+        "RED": (0, 15),
+        "ORANGE": (10, 20),
+        "YELLOW": (20, 30),
+        "GREEN": (30, 85),
+        "BLUE": (85, 140),
+        "WHITE": (140, 180)
+    }
+
+    # Determine the color based on the hue value
+    for color, (lower, upper) in color_ranges.items():
+        if lower <= hue_value <= upper:
+            return color
+
+    return "Undefined"
+
+def convertColorToRGB(color):
+    # Convert color names to RGB values
+    if color == "RED":
+        return (0, 0, 255)
+    elif color == "ORANGE":
+        return (0, 165, 255)
+    elif color == "YELLOW":
+        return (0, 255, 255)
+    elif color == "GREEN":
+        return (0, 255, 0)
+    elif color == "BLUE":
+        return (255, 0, 0)
+    elif color == "WHITE":
+        return (255, 255, 255)
     else:
-        return "Undefined"
+        return (0, 0, 0)
 
+def draw_rubiks_cube_grid(frame, cx, cy, grid_size):
+    # Draw the Rubik's Cube grid (3x3)
+    for i in range(3):
+        for j in range(3):
+            x1 = cx - grid_size // 2 + j * (grid_size // 3)
+            y1 = cy - grid_size // 2 + i * (grid_size // 3)
+            x2 = x1 + (grid_size // 3)
+            y2 = y1 + (grid_size // 3)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 2)
 
+def detectedColor(frame, cx, cy, grid_size):
+    """
+    This function returns the detected colors in the given grid.
+    """
+    matrixColor = [[], [], []]
+    origin = (cx - grid_size // 2, cy - grid_size // 2)
+    cell_size = grid_size // 3
+
+    # Detect colors in each grid cell
+    for i in range(3):
+        row = []
+        for j in range(3):
+            center_x = origin[0] + j * cell_size + cell_size // 2
+            center_y = origin[1] + i * cell_size + cell_size // 2
+            detected = getColor(frame[center_y, center_x][0])
+            row.append(detected)
+        matrixColor[i].extend(row)
+    return matrixColor
+
+def displayColors(frame, cx, cy, grid_size, matrix):
+    """
+    This function displays the detected colors in the given grid.
+    """
+    origin = (cx - grid_size // 2, cy - grid_size // 2)
+    cell_size = grid_size // 3
+
+    # Draw detected colors in each grid cell
+    for i in range(3):
+        for j in range(3):
+            color = matrix[i][j]
+            x1 = origin[0] + j * cell_size
+            y1 = origin[1] + i * cell_size
+            x2 = x1 + cell_size
+            y2 = y1 + cell_size
+            cv2.rectangle(frame, (x1, y1), (x2, y2), convertColorToRGB(color), -1)
+
+display_color_matrix = [[None, None, None], [None, None, None], [None, None, None]]
 
 while True:
     _, frame = vid.read()
@@ -29,58 +95,19 @@ while True:
     cx = width // 2
     cy = height // 2
 
-    pixel_center = hsv_frame[cy, cx]
-    hue_value = pixel_center[0]
+    draw_rubiks_cube_grid(frame, cx, cy, 300)  # Draw the Rubik's Cube grid (larger and centered)
 
-    color = getColor(hue_value)
+    key = cv2.waitKey(1)
+    if key == 32:  # Space key
+        display_color_matrix = detectedColor(frame, cx, cy, 320)
 
-    pixel_center_bgr = frame[cy, cx]
-    b, g, r = int(pixel_center_bgr[0]), int(pixel_center_bgr[1]), int(pixel_center_bgr[2])
-
-    cv2.rectangle(frame, (cx - 220, 10), (cx + 200, 120), (255, 255, 255), -1)
-    cv2.putText(frame, color, (cx - 200, 100), 0, 3, (b, g, r), 5)
-    cv2.circle(frame, (cx, cy), 5, (25, 25, 25), 3)
-
-    # création d'un tableau sur la camera de 3 lignes et 3 colonnes pour récuperer la couleur dans chaque case, represantant un couleur de 1 piece du ribik's cube
-    # 1 2 3
-    # 4 5 6
-    # 7 8 9
-
-    # ligne 1
-    cv2.rectangle(frame, (cx - 220, 130), (cx - 120, 230), (255, 255, 255), 3)
-    cv2.rectangle(frame, (cx - 120, 130), (cx - 20, 230), (255, 255, 255), 3)
-    cv2.rectangle(frame, (cx - 20, 130), (cx + 80, 230), (255, 255, 255), 3)
-    # ligne 2
-    cv2.rectangle(frame, (cx - 220, 230), (cx - 120, 330), (255, 255, 255), 3)
-    cv2.rectangle(frame, (cx - 120, 230), (cx - 20, 330), (255, 255, 255), 3)
-    cv2.rectangle(frame, (cx - 20, 230), (cx + 80, 330), (255, 255, 255), 3)
-    # ligne 3
-    cv2.rectangle(frame, (cx - 220, 330), (cx - 120, 430), (255, 255, 255), 3)
-    cv2.rectangle(frame, (cx - 120, 330), (cx - 20, 430), (255, 255, 255), 3)
-    cv2.rectangle(frame, (cx - 20, 330), (cx + 80, 430), (255, 255, 255), 3)
-
-    # quadrillage similaire a celui du dessus mais en plus petit et en bas a gauche de la page
-
-    # ligne 1
-    cv2.rectangle(frame, (cx + 120, 130), (cx + 150, 150), (255, 255, 255), 2)
-    cv2.rectangle(frame, (cx + 220, 130), (cx + 280, 180), (255, 255, 255), 2)
-    cv2.rectangle(frame, (cx + 320, 130), (cx + 420, 230), (255, 255, 255), 2)
-    # ligne 2
-    cv2.rectangle(frame, (cx + 120, 230), (cx + 220, 330), (255, 255, 255), 2)
-    cv2.rectangle(frame, (cx + 220, 230), (cx + 320, 330), (255, 255, 255), 2)
-    cv2.rectangle(frame, (cx + 320, 230), (cx + 420, 330), (255, 255, 255), 2)
-    # ligne 3
-    cv2.rectangle(frame, (cx + 120, 330), (cx + 220, 430), (255, 255, 255), 2)
-    cv2.rectangle(frame, (cx + 220, 330), (cx + 320, 430), (255, 255, 255), 2)
-    cv2.rectangle(frame, (cx + 320, 330), (cx + 420, 430), (255, 255, 255), 2)
-
-
+    displayColors(frame, width - 60, height - 60, 100, display_color_matrix)  # Display in the bottom right
 
     cv2.imshow("Color", frame)
 
-    key = cv2.waitKey(1)
-    if key == 27:  # esc
+    if key == 27:  # Esc key
         break
 
+# Release the video capture and close windows
 vid.release()
 cv2.destroyAllWindows()
